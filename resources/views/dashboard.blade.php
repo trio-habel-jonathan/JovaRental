6a9991e5499406b433cbd13d0fbb43217a12bd4b
daffa-alif <venlisiaputri21@gmail.com>
2025-03-17 11:35:41 +0700

    hapus file

--- resources/views/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>you're logged in</h1>
-
-<style>
-    h1{
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: lightblue;
-    }
-</style>
