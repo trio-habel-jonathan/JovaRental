686beca1a8f9cf6d3abc590140678ff54071e2d4
daffa-alif <venlisiaputri21@gmail.com>
2025-03-17 11:28:22 +0700

    new commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<h1>you're logged in</h1>
+
+<style>
+    h1{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: lightblue;
+    }
+</style>
