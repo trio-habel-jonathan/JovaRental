eefe4e8f7373e6e3f5d2f57b7c4caec49cca24e9
FranklinMHS01 <franklinchang0129@gmail.com>
2025-03-20 16:18:44 +0700

    UpdateFooterFrontend

--- resources/views/content/footer.blade.php
+++ resources/views/content/footer.blade.php
@@ -1,86 +1,119 @@
-<footer class="bg-white text-gray-600 py-16">
-    <div class="container mx-auto px-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-
-            <!-- Company Section -->
-            <div>
-                <h4
-                    class="text-gray-700 text-xl font-bold uppercase mb-8 relative after:block after:bg-primary after:h-0.5 after:w-12 after:absolute after:-bottom-2 transition-all duration-500">
-                    Company</h4>
-                <ul>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">About
-                            Us</a>
-                    </li>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Our
-                            Services</a></li>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Privacy
-                            Policy</a></li>
-                    <li class="mb-3"><a href="#"
-                            class="hover:text-primary transition-all duration-300">Affiliate
-                            Program</a></li>
-                </ul>
-            </div>
-
-            <!-- Get Help Section -->
-            <div>
-                <h4
-                    class="text-gray-700 text-xl font-bold uppercase mb-8 relative after:block after:bg-primary after:h-0.5 after:w-12 after:absolute after:-bottom-2 transition-all duration-500">
-                    Get Help</h4>
-                <ul>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">FAQ</a>
-                    </li>
-                    <li class="mb-3"><a href="#"
-                            class="hover:text-primary transition-all duration-300">Booking</a>
-                    </li>
-                    <li class="mb-3"><a href="#"
-                            class="hover:text-primary transition-all duration-300">Refunds</a>
-                    </li>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Order
-                            Status</a></li>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Payment
-                            Options</a></li>
-                </ul>
-            </div>
-
-            <!-- Rental Services Section -->
-            <div>
-                <h4
-                    class="text-gray-700 text-xl font-bold uppercase mb-8 relative after:block after:bg-primary after:h-0.5 after:w-12 after:absolute after:-bottom-2 transition-all duration-500">
-                    Rental Services</h4>
-                <ul>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Car
-                            Rental</a>
-                    </li>
-                    <li class="mb-3"><a href="#" class="hover:text-primary transition-all duration-300">Bike
-                            Rental</a>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- Follow Us Section -->
-            <div>
-                <h4
-                    class="text-gray-700 text-xl font-bold uppercase mb-8 relative after:block after:bg-primary after:h-0.5 after:w-12 after:absolute after:-bottom-2 transition-all duration-500">
-                    Follow Us</h4>
-                <div class="flex space-x-4">
+<footer class="bg-white text-gray-600">
+    <div>
+        <svg id="wave" style="transform:rotate(0deg); transition: 0.3s" viewBox="0 0 1440 250" version="1.1"
+            xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+                    <stop stop-color="rgba(75.933, 59.385, 139.204, 1)" offset="0%"></stop>
+                    <stop stop-color="rgba(131, 113, 199, 1)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)"
+                d="M0,175L60,154.2C120,133,240,92,360,95.8C480,100,600,150,720,158.3C840,167,960,133,1080,137.5C1200,142,1320,183,1440,175C1560,167,1680,108,1800,79.2C1920,50,2040,50,2160,45.8C2280,42,2400,33,2520,50C2640,67,2760,108,2880,112.5C3000,117,3120,83,3240,62.5C3360,42,3480,33,3600,62.5C3720,92,3840,158,3960,166.7C4080,175,4200,125,4320,95.8C4440,67,4560,58,4680,75C4800,92,4920,133,5040,133.3C5160,133,5280,92,5400,83.3C5520,75,5640,100,5760,95.8C5880,92,6000,58,6120,50C6240,42,6360,58,6480,66.7C6600,75,6720,75,6840,62.5C6960,50,7080,25,7200,12.5C7320,0,7440,0,7560,29.2C7680,58,7800,117,7920,137.5C8040,158,8160,142,8280,150C8400,158,8520,192,8580,208.3L8640,225L8640,250L8580,250C8520,250,8400,250,8280,250C8160,250,8040,250,7920,250C7800,250,7680,250,7560,250C7440,250,7320,250,7200,250C7080,250,6960,250,6840,250C6720,250,6600,250,6480,250C6360,250,6240,250,6120,250C6000,250,5880,250,5760,250C5640,250,5520,250,5400,250C5280,250,5160,250,5040,250C4920,250,4800,250,4680,250C4560,250,4440,250,4320,250C4200,250,4080,250,3960,250C3840,250,3720,250,3600,250C3480,250,3360,250,3240,250C3120,250,3000,250,2880,250C2760,250,2640,250,2520,250C2400,250,2280,250,2160,250C2040,250,1920,250,1800,250C1680,250,1560,250,1440,250C1320,250,1200,250,1080,250C960,250,840,250,720,250C600,250,480,250,360,250C240,250,120,250,60,250L0,250Z">
+            </path>
+            <defs>
+                <linearGradient id="sw-gradient-1" x1="0" x2="0" y1="1" y2="0">
+                    <stop stop-color="rgba(73.518, 52.17, 155.142, 1)" offset="0%"></stop>
+                    <stop stop-color="rgba(131, 113, 199, 1)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <path style="transform:translate(0, 50px); opacity:0.9" fill="url(#sw-gradient-1)"
+                d="M0,75L60,70.8C120,67,240,58,360,50C480,42,600,33,720,62.5C840,92,960,158,1080,154.2C1200,150,1320,75,1440,54.2C1560,33,1680,67,1800,91.7C1920,117,2040,133,2160,141.7C2280,150,2400,150,2520,150C2640,150,2760,150,2880,145.8C3000,142,3120,133,3240,108.3C3360,83,3480,42,3600,29.2C3720,17,3840,33,3960,66.7C4080,100,4200,150,4320,154.2C4440,158,4560,117,4680,83.3C4800,50,4920,25,5040,37.5C5160,50,5280,100,5400,129.2C5520,158,5640,167,5760,141.7C5880,117,6000,58,6120,37.5C6240,17,6360,33,6480,37.5C6600,42,6720,33,6840,58.3C6960,83,7080,142,7200,158.3C7320,175,7440,150,7560,145.8C7680,142,7800,158,7920,175C8040,192,8160,208,8280,200C8400,192,8520,158,8580,141.7L8640,125L8640,250L8580,250C8520,250,8400,250,8280,250C8160,250,8040,250,7920,250C7800,250,7680,250,7560,250C7440,250,7320,250,7200,250C7080,250,6960,250,6840,250C6720,250,6600,250,6480,250C6360,250,6240,250,6120,250C6000,250,5880,250,5760,250C5640,250,5520,250,5400,250C5280,250,5160,250,5040,250C4920,250,4800,250,4680,250C4560,250,4440,250,4320,250C4200,250,4080,250,3960,250C3840,250,3720,250,3600,250C3480,250,3360,250,3240,250C3120,250,3000,250,2880,250C2760,250,2640,250,2520,250C2400,250,2280,250,2160,250C2040,250,1920,250,1800,250C1680,250,1560,250,1440,250C1320,250,1200,250,1080,250C960,250,840,250,720,250C600,250,480,250,360,250C240,250,120,250,60,250L0,250Z">
+            </path>
+            <defs>
+                <linearGradient id="sw-gradient-2" x1="0" x2="0" y1="1" y2="0">
+                    <stop stop-color="rgba(98.713, 76.81, 182.463, 1)" offset="0%"></stop>
+                    <stop stop-color="rgba(152.733, 140.645, 198.401, 1)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <path style="transform:translate(0, 100px); opacity:0.8" fill="url(#sw-gradient-2)"
+                d="M0,0L60,12.5C120,25,240,50,360,70.8C480,92,600,108,720,120.8C840,133,960,142,1080,133.3C1200,125,1320,100,1440,112.5C1560,125,1680,175,1800,183.3C1920,192,2040,158,2160,145.8C2280,133,2400,142,2520,137.5C2640,133,2760,117,2880,91.7C3000,67,3120,33,3240,50C3360,67,3480,133,3600,141.7C3720,150,3840,100,3960,70.8C4080,42,4200,33,4320,29.2C4440,25,4560,25,4680,58.3C4800,92,4920,158,5040,191.7C5160,225,5280,225,5400,208.3C5520,192,5640,158,5760,141.7C5880,125,6000,125,6120,120.8C6240,117,6360,108,6480,87.5C6600,67,6720,33,6840,20.8C6960,8,7080,17,7200,54.2C7320,92,7440,158,7560,158.3C7680,158,7800,92,7920,83.3C8040,75,8160,125,8280,129.2C8400,133,8520,92,8580,70.8L8640,50L8640,250L8580,250C8520,250,8400,250,8280,250C8160,250,8040,250,7920,250C7800,250,7680,250,7560,250C7440,250,7320,250,7200,250C7080,250,6960,250,6840,250C6720,250,6600,250,6480,250C6360,250,6240,250,6120,250C6000,250,5880,250,5760,250C5640,250,5520,250,5400,250C5280,250,5160,250,5040,250C4920,250,4800,250,4680,250C4560,250,4440,250,4320,250C4200,250,4080,250,3960,250C3840,250,3720,250,3600,250C3480,250,3360,250,3240,250C3120,250,3000,250,2880,250C2760,250,2640,250,2520,250C2400,250,2280,250,2160,250C2040,250,1920,250,1800,250C1680,250,1560,250,1440,250C1320,250,1200,250,1080,250C960,250,840,250,720,250C600,250,480,250,360,250C240,250,120,250,60,250L0,250Z">
+            </path>
+        </svg>
+        <div
+            class="bg-primary text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full px-16 py-12 montserrat-font">
+            <div class="space-y-6">
+                <h1 class="text-4xl font-bold">JovaRental</h1>
+                <p>Platform penyedia rental kendaraan terpercaya dengan pilihan beragam dan harga terbaik. Nikmati
+                    perjalanan nyaman dan aman bersama kami!</p>
+                <div class="flex gap-4">
                     <a href="#"
-                        class="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center hover:bg-primary hover:scale-110 hover:text-white transition-all duration-300"><i
+                        class="w-10 h-10 rounded-full bg-transparent border border-white flex items-center justify-center hover:bg-white hover:scale-110 hover:text-primary transition-all duration-300"><i
                             class="fa-brands fa-facebook-f"></i></a>
                     <a href="#"
-                        class="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center hover:bg-primary hover:scale-110 hover:text-white transition-all duration-300"><i
+                        class="w-10 h-10 rounded-full bg-transparent border border-white flex items-center justify-center hover:bg-white hover:scale-110 hover:text-primary transition-all duration-300"><i
                             class="fa-brands fa-twitter"></i></a>
                     <a href="#"
-                        class="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center hover:bg-primary hover:scale-110 hover:text-white transition-all duration-300"><i
+                        class="w-10 h-10 rounded-full bg-transparent border border-white flex items-center justify-center hover:bg-white hover:scale-110 hover:text-primary transition-all duration-300"><i
                             class="fa-brands fa-instagram"></i></a>
                     <a href="#"
-                        class="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center hover:bg-primary hover:scale-110 hover:text-white transition-all duration-300">
+                        class="w-10 h-10 rounded-full bg-transparent border border-white flex items-center justify-center hover:bg-white hover:scale-110 hover:text-primary transition-all duration-300">
                         <i class="fa-brands fa-linkedin"></i></a>
                 </div>
             </div>
-        </div>
-        <!-- Copyright -->
-        <div class="text-center text-gray-500 mt-12">
-            © 2025 JovaRental. All rights reserved.
+            <div class="col-span-2 flex flex-col space-y-6">
+                <h1 class="text-4xl font-bold">Ada yang bisa kami bantu?</h1>
+                <p class="text-justify">Ada yang bisa kami bantu? Jangan ragu untuk menghubungi kami jika Anda memiliki
+                    pertanyaan,
+                    membutuhkan bantuan, atau ingin mendapatkan informasi lebih lanjut. Tim kami siap memberikan solusi
+                    terbaik untuk Anda.</p>
+                <ul class="flex font-semibold flex-wrap justify-between items-center">
+                    <li class="flex gap-2 items-center">
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12 17.5H12.01M8.2 22H15.8C16.9201 22 17.4802 22 17.908 21.782C18.2843 21.5903 18.5903 21.2843 18.782 20.908C19 20.4802 19 19.9201 19 18.8V5.2C19 4.07989 19 3.51984 18.782 3.09202C18.5903 2.71569 18.2843 2.40973 17.908 2.21799C17.4802 2 16.9201 2 15.8 2H8.2C7.0799 2 6.51984 2 6.09202 2.21799C5.71569 2.40973 5.40973 2.71569 5.21799 3.09202C5 3.51984 5 4.0799 5 5.2V18.8C5 19.9201 5 20.4802 5.21799 20.908C5.40973 21.2843 5.71569 21.5903 6.09202 21.782C6.51984 22 7.07989 22 8.2 22ZM12.5 17.5C12.5 17.7761 12.2761 18 12 18C11.7239 18 11.5 17.7761 11.5 17.5C11.5 17.2239 11.7239 17 12 17C12.2761 17 12.5 17.2239 12.5 17.5Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        0812345678
+                    </li>
+                    <li class="flex gap-2 items-center">
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M16 7.99999V13C16 13.7956 16.3161 14.5587 16.8787 15.1213C17.4413 15.6839 18.2043 16 19 16C19.7956 16 20.5587 15.6839 21.1213 15.1213C21.6839 14.5587 22 13.7956 22 13V12C21.9999 9.74302 21.2362 7.55247 19.8333 5.78452C18.4303 4.01658 16.4705 2.77521 14.2726 2.26229C12.0747 1.74936 9.76793 1.99503 7.72734 2.95936C5.68676 3.92368 4.03239 5.54995 3.03325 7.57371C2.03411 9.59748 1.74896 11.8997 2.22416 14.1061C2.69936 16.3125 3.90697 18.2932 5.65062 19.7263C7.39428 21.1593 9.57143 21.9603 11.8281 21.9991C14.0847 22.0379 16.2881 21.3122 18.08 19.94M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79085 9.79086 7.99999 12 7.99999C14.2091 7.99999 16 9.79085 16 12Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        jovarental@gmail.com
+                    </li>
+                    <li class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="lucide lucide-instagram">
+                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                        </svg>
+                        jovarental_id
+                    </li>
+                    <li class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="lucide lucide-facebook">
+                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                        </svg>
+                        JovaRental
+                    </li>
+                </ul>
+                <a href="" class="bg-white rounded-md w-fit px-6 py-2 text-primary font-bold">
+                    Contact Us
+                </a>
+            </div>
+            <div class="space-y-6">
+                <h1 class="text-4xl font-bold">Our Apps</h1>
+                <ul class="flex gap-4">
+                    <li>
+                        <img draggable="false" class="rounded-lg w-44"
+                            src="https://i.pinimg.com/736x/a2/dc/17/a2dc1725c52455787f90c320d47e1acf.jpg"
+                            alt="">
+                    </li>
+                    <li>
+                        <img draggable="false" class="rounded-lg w-44"
+                            src="https://i.pinimg.com/736x/ae/0d/5b/ae0d5b9d14f0c1d21bb997adf31e4e25.jpg"
+                            alt="">
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
