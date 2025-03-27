9f0591692270bb998395893e08fc37b946804f1c
Habib <heandsome083@gmail.com>
2025-03-27 12:29:39 +0700

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'VerifiedEntity' => \App\Http\Middleware\VerifikasiEntitasMiddleware::class
+            'VerifiedEntity' => \App\Http\Middleware\VerifikasiEntitasMiddleware::class,
+            'CheckRole' => \App\Http\Middleware\CheckRoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
